6d2ad15045aaa466c14fc65334b5dcf67773b204
Nicolas Viruel <nicolasviruel@gmail.com>
2025-11-25 18:05:16 -0300

    Se añadieron mecanismos de gestión y actualización del estado del personaje.
    - Se introdujeron los atributos de energía, comida y sed para la clase Personaje.
    - Se implementaron métodos para actualizar estos atributos y mostrar las barras de estado correspondientes en la pantalla.
    - Se añadió lógica para disminuir la energía según los niveles de comida y sed.
    - Se actualizó el bucle principal para gestionar las actualizaciones de estado y mostrar los valores actuales en la pantalla.
    - Se definieron constantes para los valores máximos y los colores de las barras de estado en constants.py.

--- constants.py
+++ constants.py
@@ -18,3 +18,17 @@ PINK = (255, 192, 203)
 GRAY = (128, 128, 128)
 BROWN = (165, 42, 42)
 CYAN = (0, 255, 255)
+
+# Barras de estado
+MAX_ENERGY = 100
+MAX_FOOD = 100
+MAX_THIRST = 100
+
+# Colores para las barras de etado
+ENERGY_COLOR = (255, 255, 0) # Amarillo
+FOOD_COLOR = (255, 165, 0) # Naranja
+THIRST_COLOR = (0, 191, 255) # Azul
+BAR_BACKGROUND = (100, 100, 100) # Gris oscuro
+
+# Intervalo de tiempo
+STATUS_UPDATE_INTERVAL = 1000
