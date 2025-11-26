b207f9cf10ad5256e4f6c0c5548d3b982a8c048f
nicolas <nicolas.viruel@phinxlab.com>
2025-11-26 16:36:20 -0300

    Se agrega el cilo de Dia y Noche

--- main.py
+++ main.py
@@ -23,6 +23,7 @@ def main():
 
     while True:
         dt = clock.tick(60)
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
@@ -47,6 +48,9 @@ def main():
         if keys[pygame.K_DOWN]:
             character.move(0, 5, world)
 
+        # Actualizar el tiempo del dia
+        world.update_time(dt)
+
         status_update_timer += dt
         if status_update_timer >= constants.STATUS_UPDATE_INTERVAL:
             character.update_status()
@@ -67,9 +71,15 @@ def main():
         energy_text = font.render(f"Energy: {int(character.energy)}", True, constants.WHITE)
         food_text = font.render(f"Food: {int(character.food)}", True, constants.WHITE)
         thirst_text = font.render(f"Thirst: {int(character.thirst)}", True, constants.WHITE)
-        screen.blit(energy_text, (10, constants.HEIGHT - 70))
-        screen.blit(food_text, (10, constants.HEIGHT - 45))
-        screen.blit(thirst_text, (10, constants.HEIGHT - 20))
+        #Indicador de tiempo
+        time_of_day = (world.current_time / constants.DAY_LENGTH) * 24
+        time_text = font.render(f"Time: {int(time_of_day):02d}:00", True, constants.WHITE)
+        
+        
+        screen.blit(energy_text, (10, constants.HEIGHT - 90))
+        screen.blit(food_text, (10, constants.HEIGHT - 65))
+        screen.blit(thirst_text, (10, constants.HEIGHT - 40))
+        screen.blit(time_text, (10, constants.HEIGHT - 15))
         
         pygame.display.flip()
 
